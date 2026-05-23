b02ac69d7beb02c7c66b48dd5c0f23389af08b6f
Deepak Kumar <deepak.kumar6747@hotmail.com>
2026-05-23 15:11:25 +0530

    Implemented Retry Analyzer for failed API test execution handling

---
+++ src/test/java/retry/Retry.java
@@ -0,0 +1,24 @@
+package retry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer {
+
+    private int count = 0;
+
+    private static final int maxRetry = 2;
+
+    @Override
+    public boolean retry(ITestResult result) {
+
+        if (count < maxRetry) {
+
+            count++;
+
+            return true;
+        }
+
+        return false;
+    }
+}
